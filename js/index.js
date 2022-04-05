7c932d82c45c69aa092968fc913c72c622390168
kristijan_00 <kristijandjakovic10@gmail.com>
2022-04-05 16:10:06 +0200

    Most of the features fixed

--- js/index.js
+++ js/index.js
@@ -1,14 +1,46 @@
+let aTagsInNav = document.querySelectorAll('nav a');
+let liTagsinNav = document.querySelectorAll('nav li');
+let navBar = document.querySelector('.navBar');
 document.querySelector('nav').addEventListener('click', function (event) {
     setTimeout(function () {
         let route = location.href.substring(27);
-        let aTagsInNav = document.querySelectorAll('nav a');
-        console.log(route);
-        for (let aTag of aTagsInNav) {
-            aTag.classList.remove('active');
-            let href = aTag.getAttribute('href');
-            if (href === route) {
-                aTag.classList.add('active');
-            }
-        }
-    }, 30);
+        addClass(route);
+    }, 40);
 });
+
+let sections = document.querySelectorAll('.section')
+window.onscroll = () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+            console.log(current);
+        }
+    });
+    addClass(current);
+};
+
+var myCarousel = document.querySelector('#work-carousel');
+var carousel = new bootstrap.Carousel(myCarousel);
+
+function addClass(route) {
+    if (route == 'about' || route == 'work') {
+        navBar.classList.add('change');
+    } else {
+        navBar.classList.remove('change');
+    }
+    for (let liTag of liTagsinNav) {
+        liTag.classList.remove('active');
+    }
+    for (let aTag of aTagsInNav) {
+        aTag.classList.remove('active');
+        let href = aTag.getAttribute('href');
+
+        if (href === "#" + route) {
+            aTag.classList.add('active');
+            aTag.parentElement.closest('li').classList.add('active');
+        }
+    }
+}
